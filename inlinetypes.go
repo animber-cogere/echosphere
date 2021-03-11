62200af6f4947ba07a2af6da112da6d612cb9b14
animber-coder <176597279+animber-coder@users.noreply.github.com>
2021-03-11 14:30:06 +0100

    Update copyright notice

--- inlinetypes.go
+++ inlinetypes.go
@@ -1,6 +1,6 @@
 /*
  * Echosphere
- * Copyright (C) 2019  Michele Dimaggio
+ * Copyright (C) 2018-2021  Michele Dimaggio
  *
  * Echosphere is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by

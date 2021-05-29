63e828938d92cb844bfa2f4733cffcd8ed708391
animber-coder <176597279+animber-coder@users.noreply.github.com>
2021-05-29 17:04:26 +0200

    Port all methods to v3 and reorganize everything

---
+++ games.go
@@ -0,0 +1,29 @@
+/*
+ * Echosphere
+ * Copyright (C) 2021  The Echosphere Devs
+ *
+ * Echosphere is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Echosphere is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package echosphere
+
+// Game represents a game.
+type Game struct {
+	Title        string          `json:"title"`
+	Description  string          `json:"description"`
+	Photo        []PhotoSize     `json:"photo"`
+	Text         string          `json:"text,omitempty"`
+	TextEntities []MessageEntity `json:"text_entities,omitempty"`
+	Animation    Animation       `json:"animation,omitempty"`
+}

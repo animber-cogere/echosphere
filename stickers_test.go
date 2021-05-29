e2f7653259c6febd4332e738f1b5d01606c16e41
animber-coder <176597279+animber-coder@users.noreply.github.com>
2021-05-30 01:05:08 +0200

    Port all tests to v3 and reorganize everything

---
+++ stickers_test.go
@@ -0,0 +1,48 @@
+/*
+ * Echosphere
+ * Copyright (C) 2021  The Echosphere Devs
+ *
+ * Echosphere is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Echosphere is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package echosphere
+
+import "testing"
+
+func TestSendSticker(t *testing.T) {
+    resp, err := api.SendSticker(
+        stickerID,
+        chatID,
+        nil,
+    )
+
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    if !resp.Ok {
+        t.Fatal(resp.ErrorCode, resp.Description)
+    }
+}
+
+func TestGetStickerSet(t *testing.T) {
+    resp, err := api.GetStickerSet("RickAndMorty")
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    if !resp.Ok {
+        t.Fatal(resp.ErrorCode, resp.Description)
+    }
+}

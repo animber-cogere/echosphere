30b5c224980732c4b3793a95113027dfcefd144a
animber-coder <176597279+animber-coder@users.noreply.github.com>
2022-01-06 15:57:49 +0100

    apierror.go: add tests

---
+++ apierror_test.go
@@ -0,0 +1,17 @@
+package echosphere
+
+import "testing"
+
+var a APIError
+
+func TestErrorCode(_ *t.Testing) {
+	a.ErrorCode()
+}
+
+func TestDescription(_ *t.Testing) {
+	a.Description()
+}
+
+func TestError(_ *t.Testing) {
+	a.Error()
+}
